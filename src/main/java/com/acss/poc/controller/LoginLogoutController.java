5dfb2fa71bbeb35ceab5c4322bfbb77545a2ae91
ultragaylord <vargas.gail@gmail.com>
2014-02-22 19:25:30 +0800

    Removed Unused Imports

--- src/main/java/com/acss/poc/controller/LoginLogoutController.java
+++ src/main/java/com/acss/poc/controller/LoginLogoutController.java
@@ -8,8 +8,6 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Handles and retrieves the login or denied page depending on the URI template
